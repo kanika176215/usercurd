a7408190d8fb4e5797ee57578477938a90aef211
kanika176215 <kanika176215@gmail.com>
2023-02-16 16:53:15 +0530

    commit

--- express_js/app/routes/user.js
+++ express_js/app/routes/user.js
@@ -1,22 +1,36 @@
-const express= require('express');
-const router= express.Router();
-const userController= require('../controllers/user')
+const express=require('express');
+const router=express.Router();
+const userController = require('../controllers/user')
 
-router.post('/user', (req,res,next)=>{
-    userController.create(req,res,next)
+//--------------------------------create----------------------------------post----------------------------------
+router.post('/user',(req, res, next)=>{
+    userController.create(req, res, next)
+});
+
+
+//--------------------------------get----------------------------------find-------------------------------------
+router.get('/user',(req, res, next)=>{
+    userController.find(req, res, next)
 });
 
 
 
-router.get('/user',(req, res, next)=> {
-    userController.find(req,res, next)
+//--------------------------------get1----------------------------------findOne--------------------------------------
+router.get('/user/:userId',(req, res, next)=>{
+    userController.findOne(req, res, next)
+});
 
 
+//--------------------------------update----------------------------------put---------------------------
+router.put('/user/:userId',(req, res, next)=>{
+    userController.update(req, res, next)
+});
 
 
+//--------------------------------delete----------------------------------
+router.delete('/user/:userId', (req, res ,next)=>{
+userController.delete(req, res, next)
 });
 
 
 module.exports=router
-
-
--- express_js/app/services/user.js
+++ express_js/app/services/user.js
@@ -3,35 +3,66 @@ const userData = require('../data/user.json')
 module.exports={
 
 
-    async create(data) {
-
-
-        
-
-        
-        let userId =userData[userData.length -1].id +1
+   //--------------------------------create----------------------------------post----------------------------------
+     async create(data){
+        let userId= userData[userData.length-1].id+1
         const muserData ={
-            id : userId,
-            name:data.name,
-            Email:data.Email,
-           
+        id: userId,
+        name:data.name,
+        Email:data.Email
+      
+      
         }
         userData.push(muserData)
         return muserData
-    },
+     },
 
 
-    async find() {
-
-        //fetch Data from database
-
-        const user =userData
-        return user
-
-
-    
-    },
+     //--------------------------------get----------------------------------find-------------------------------------
+     async find(){
+     const user =userData
+     return user
+     },
 
 
+     //--------------------------------get1----------------------------------findOne--------------------------------------
+     async findOne(userId){
+      const user= userData.find((singleuser)=>{
+         if(singleuser.id==userId){
+            return singleuser
+         }
+      })
+      return user
+     },
+
+
+
+     //--------------------------------update----------------------------------put---------------------------
+async update(userId,data){
+let updateuser={}
+userData.forEach((singleuser)=>{
+   if(singleuser.id== userId){
+      singleuser.body=data.body
+      singleuser.name=data.name
+      singleuser.Email=data.Email
+      updateuser=singleuser
+   }
+})
+return updateuser
+} ,
+
+
+
+//--------------------------------delete----------------------------------
+async delete(userId){
+const userIndex= userData.findIndex((singleuser)=>{
+   if(singleuser.id==userId){
+      return singleuser
+   }
+})
+
+userData.splice(userIndex, 1)
+return "ok"
+}
 
 }
